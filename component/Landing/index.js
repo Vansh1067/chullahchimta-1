9ac704daf6c9181c94ee1fc7542540794b0025e8
Vansh <vanshtandon1067@gmail.com>
2021-10-09 20:07:53 +0530

    Change spelling

--- component/Landing/index.js
+++ component/Landing/index.js
@@ -3,6 +3,8 @@ import styles from './landing.module.css'
 
 const Landing =()=>{
     return (<div className={styles.container} id="home">
+
+      
       <img src="/images/Landing.png" className={styles.image}/>
         <img src="/images/Tag.svg" className={styles.tag}/>
  
